ed28d012a8f4b78c4e19ae9f9fe78dc566865430
novedice <Natalia.kitesurf@gmail.com>
2023-02-06 23:42:23 +0000

    feat: render game page with implementation of category filter

---
+++ client/src/game-content/allGames.ts
@@ -0,0 +1,39 @@
+export const allGames = [
+  {
+    name: 'Reaction Time',
+    category: 'Speed',
+  },
+  {
+    name: 'Sequence memory',
+    category: 'Memory',
+  },
+  {
+    name: 'Aim Trainer',
+    category: 'Speed',
+  },
+  {
+    name: 'Chimp test',
+    category: 'Problrm solving',
+  },
+
+  {
+    name: 'number Memory',
+    category: 'Memory',
+  },
+  {
+    name: 'Verbal memory',
+    category: 'Memory',
+  },
+  {
+    name: 'Visual memory',
+    category: 'Memory',
+  },
+  {
+    name: 'Typing',
+    category: 'Speed',
+  },
+  {
+    name: 'Pixel logic',
+    category: 'Problem Solving',
+  },
+];
--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -1,4 +1,39 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { GameCategories } from '../components/GameCategories';
+import { allGames } from '../game-content/allGames';
+
 // import { Route, Routes } from 'react-router-dom';
 export function GamePage() {
-  return <div>Game Page</div>;
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get('category');
+
+  const filteredGames = () => {
+    let filtGames = allGames;
+    if (category !== 'All categories' && category) {
+      filtGames = allGames.filter((game) => game.category === category);
+    }
+    return filtGames;
+  };
+  return (
+    <>
+      <div className="m-3 flex h-full ">
+        <aside className="width-[20%] mr-5 border">
+          <GameCategories />
+        </aside>
+        <main className="flex w-[80%] justify-around">
+          {filteredGames().map((game) => {
+            return (
+              <React.Fragment key={game.name}>
+                <div>
+                  <p>{game.name}</p>
+                  <p>{game.category}</p>
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
 }
