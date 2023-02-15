c3f38129e9a8820fbbb9146264fe34db22b6bd2f
sluzerpp <bistboy@list.ru>
2023-02-15 19:10:54 +0300

    feat:  categories requests

---
+++ client/src/api/category-requests.ts
@@ -0,0 +1,35 @@
+import { BaseUrl, categories, users } from "./constants";
+import { $host } from "./http";
+
+export const createCategory = async (category: string) => {
+  try {
+    const response = await $host.post<IResultResponse>(`${BaseUrl}/${users}/${categories}`, category, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getCategory = async (category: string) => {
+  try {
+    const response = await $host.get<IResultResponse>(`${BaseUrl}/${users}/${categories}`, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const DeleteCategory = async (id: string) => {
+  try {
+    const response = await $host.delete<IResultResponse>(`${BaseUrl}/${users}/${categories}/${id}`, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
