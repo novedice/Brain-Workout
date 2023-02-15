72f686e64faab6db43c1c5d47d60ebfd75fc7e94
sluzerpp <bistboy@list.ru>
2023-02-15 19:13:20 +0300

    refactor: remove comments

--- client/src/api/user-requests.ts
+++ client/src/api/user-requests.ts
@@ -12,29 +12,6 @@ interface IDataUser {
   nickname?: string;
 }
 
-// export const getUser = () => {
-//   const [cookie] = useCookies(['token']);
-//   const [user, setUser] = useState();
-//   const [error, setError] = useState('');
-//   const getUs = async () => {
-//     try {
-//       const response = await axios.get(`${BaseUrl}/${users}/${cookie}`, {
-//         withCredentials: true,
-//       });
-//       if (response) {
-//         setUser(response.data);
-//       }
-//     } catch (e) {
-//       setError((e as AxiosError).message);
-//     }
-//   };
-//   useEffect(() => {
-//     getUs();
-//   });
-
-//   return { user, error };
-// };
-
 export const updateUser = async (data: IDataUser) => {
   try {
     const response = await axios.put<IToken>(`${BaseUrl}/${users}/`, data, {
@@ -62,55 +39,23 @@ export const registrAuthUser = async (
   data: IDataUser,
   action: 'registration' | 'login'
 ) => {
-  // const [token, setToken] = useState();
-  // const [error, setError] = useState('');
-  // const { reEvaluateToken } = useJwt('');
-  // const [, setCookie] = useCookies(['token']);
 
   try {
     const response = await $host.post<IToken>(`${BaseUrl}/${users}/${action}`, data, {
       withCredentials: true,
     })
-    // const response = await axios.post(`${BaseUrl}/${users}/${action}`, data, {
-    //   withCredentials: true,
-    // });
+
     return response.data;
 
-    // if (response) {
-    // return response.data
-    // setToken(response.data);
-    // setCookie('token', response.data);
-    // reEvaluateToken(response.data);
-    // }
   } catch (e) {
     if (e) {
-      // setError((e as AxiosError).message);
       console.log(e);
     }
   }
 };
 
-// export const authorizateUser = async (data: IDataUser) => {
-//   const [token, setToken] = useState();
-//   const [error, setError] = useState('');
-//   try {
-//     const response = await axios.post(`${BaseUrl}/${users}/${login}`, data);
-//     if (response) {
-//       setToken(response.data);
-//     }
-//   } catch (e) {
-//     if (e) {
-//       setError((e as AxiosError).message);
-//       return error;
-//     }
-//   }
-//   return token;
-// };
 
 export const checkToken = async () => {
-  // const [newToken, setNewToken] = useState();
-  // const [error, setError] = useState('');
-  // const [cookie, setCookie] = useCookies(['token']);
   let error;
   try {
     const response = await $host.get<IToken>(
@@ -118,15 +63,9 @@ export const checkToken = async () => {
       { withCredentials: true }
     );
     return response.data;
-    // if (response) {
-
-    //   // setNewToken(response.data);
-    //   // setCookie('token', response.data);
-    // }
   } catch (e) {
     error = (e as AxiosError).message;
     console.log(error);
     return null;
   }
-  
 };
