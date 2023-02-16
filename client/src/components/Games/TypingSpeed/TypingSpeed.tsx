16e3532f45fcb47958c42b2453ae7c36e916e93c
sergeibuiko <sergeibuiko1@gmail.com>
2023-02-16 16:23:06 +0300

    feat: add interface

--- client/src/components/Games/TypingSpeed/TypingSpeed.tsx
+++ client/src/components/Games/TypingSpeed/TypingSpeed.tsx
@@ -1,7 +1,21 @@
 // export function TypingSpeed() {
 //   return <p>TypingSpeed</p>;
 // }
-
+interface IState {
+  type: string;
+  content: Array<string>;
+}
 export function TypingSpeed() {
-  return <div>TypingSpeed</div>;
+  const state: IState = {
+    type: 'e2ndunmund udu2nun unu 2d2d2',
+    content: [],
+  };
+  return (
+    <div>
+      <h1>Typing Speed Test</h1>
+      <h3>How many words per minute can you type?</h3>
+      <p>{state.type}</p>
+      <input type="text" name="" id="" />
+    </div>
+  );
 }
