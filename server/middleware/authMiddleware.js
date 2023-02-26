fb51ac11a626e3041398f37a9c339a5f5ef88555
novedice <Natalia.kitesurf@gmail.com>
2023-02-26 21:51:18 +0000

    fix: additional log to middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,25 +1,28 @@
-const jwt = require('jsonwebtoken');
-const sessionController = require('../controller/sessionController');
+const jwt = require("jsonwebtoken");
+const sessionController = require("../controller/sessionController");
 
 module.exports = async function (req, res, next) {
-  if (req.method == 'OPTIONS') {
+  if (req.method == "OPTIONS") {
     next();
   }
   try {
-    const token = req.cookies.auth.split(' ')[1] // Bearer eq4q23qe32...
+    console.log(req.cookies);
+    console.log(req.auth);
+
+    const token = req.cookies.auth.split(" ")[1]; // Bearer eq4q23qe32...
     if (!token) {
-      return res.status(401).json({message: 'User is not authorized!' });
+      return res.status(401).json({ message: "User is not authorized!" });
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     const session = await sessionController.get(decoded.sessionId);
     if (session.token !== token) {
-      return res.status(401).json({message: 'User is not authorized!'});
+      return res.status(401).json({ message: "User is not authorized!" });
     }
-    req.token = token; 
+    req.token = token;
     req.user = decoded;
-    
+
     next();
   } catch (e) {
-    res.status(401).json({message: e.message});
+    res.status(401).json({ message: e.message });
   }
-}
+};
