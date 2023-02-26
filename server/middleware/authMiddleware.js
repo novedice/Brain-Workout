d331589543f57142471bfd7ecf48148be85bc7af
novedice <Natalia.kitesurf@gmail.com>
2023-02-26 22:38:25 +0000

    add: another  testing

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -12,10 +12,12 @@ module.exports = async function (req, res, next) {
     console.log("headers auth1:", req.headers.auth1);
 
     const token = req.headers.auth.split(" ")[1]; // Bearer eq4q23qe32...
+    console.log("token in middleware", token);
     if (!token) {
       return res.status(401).json({ message: "User is not authorized!" });
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    console.log("decoded", decoded);
     const session = await sessionController.get(decoded.sessionId);
     if (session.token !== token) {
       return res.status(401).json({ message: "User is not authorized!" });
